447716abb21967ccfbf25bbd31dbce403d424615
keivys Rodríguez <keivys17@gmail.com>
2023-04-11 16:16:10 -0500

    App ok

--- src/main/java/com/jose/restapialura/RestApiAluraApplication.java
+++ src/main/java/com/jose/restapialura/RestApiAluraApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestApiAluraApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestApiAluraApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiAluraApplication.class, args);
+    }
 
 }
